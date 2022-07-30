9293ca37382bb93f528e8b64762c748eb257a23f
Lwazi Ndlovu <42185877+lwazin@users.noreply.github.com>
2022-07-30 10:08:37 +0200

    update :: done with badges! Map next..

--- pages/MagicDragon_.tsx
+++ pages/MagicDragon_.tsx
@@ -3,7 +3,18 @@ import {
   faInstagram,
   faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCartShopping,
+  faCheck,
+  faCheckCircle,
+  faEarthAfrica,
+  faHeadSideMask,
+  faMusic,
+  faPeopleGroup,
+  faShield,
+  faTv,
+  faTvAlt,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface MagicDragon_Props {}
@@ -224,8 +235,12 @@ const MagicDragon_ = ({}: MagicDragon_Props) => {
         <div
           className={`w-full h-full flex flex-row justify-center items-center pt-[150px] bg-[#2e2c2d]/60 absolute top-0 right-0 `}
         >
-          <div className={`flex flex-col justify-center items-center pt-[250px]`}>
-            <p className={`text-[45px] text-white/70 font-bold Oswald_ mb-4 text-right tracking-[1px] w-[400px]`}>
+          <div
+            className={`flex flex-col justify-center items-center pt-[250px]`}
+          >
+            <p
+              className={`text-[45px] text-white/70 font-bold Oswald_ mb-4 text-right tracking-[1px] w-[400px]`}
+            >
               Work break on us!
             </p>
             <div
@@ -238,11 +253,150 @@ const MagicDragon_ = ({}: MagicDragon_Props) => {
               reprehenderit ullamco.
             </div>
           </div>
-          <div className={`w-[600px] h-[320px] rounded-[2px] relative bottom-[65px] ml-6 overflow-hidden`}>
-            <img src={`/assets/images/SmokeRoom.jpg`} className={`w-full object-cover relative bottom-[400px] opacity-50`}/>
+          <div
+            className={`w-[600px] h-[320px] rounded-[2px] relative bottom-[65px] ml-6 overflow-hidden`}
+          >
+            <img
+              src={`/assets/images/SmokeRoom.jpg`}
+              className={`w-full object-cover relative bottom-[400px] opacity-50`}
+            />
           </div>
-          <div className={`w-[300px] h-[320px] rounded-[2px] relative bottom-[65px] ml-4 overflow-hidden`}>
-            <img src={`https://images.pexels.com/photos/7667812/pexels-photo-7667812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`} className={`W-full object-cover relative bottom-[70px] opacity-70`}/>
+          <div
+            className={`w-[300px] h-[320px] rounded-[2px] relative bottom-[65px] ml-4 overflow-hidden`}
+          >
+            <img
+              src={`https://images.pexels.com/photos/7667812/pexels-photo-7667812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`}
+              className={`W-full object-cover relative bottom-[70px] opacity-70`}
+            />
+          </div>
+        </div>
+      </div>
+      <div
+        className={`w-full min-h-[250px] bg-black relative flex flex-col justify-center items-center`}
+      >
+        <div
+          className={`w-full h-[100px] relative flex flex-col justify-center items-center bg-[#2e2c2d]/60 pt-[50px]`}
+        >
+          <p className={`text-[30px] font-black text-white/70 mb-2`}>
+            Our Private Rooms.. High Standards!
+          </p>
+          <p
+            className={`w-[30%] mx-auto text-[14px] text-white/50 Work_ font-light text-center relative`}
+          >
+            Magna irure dolor labore nulla eu commodo deserunt labore duis
+            laboris irure. Ipsum eu laborum ex aliqua sunt est ulla.
+          </p>
+        </div>
+        <div
+          className={`w-full h-full flex flex-row justify-center items-center bg-[#2e2c2d]/60 py-[25px] pt-[50px]`}
+        >
+          <div
+            className={`w-[250px] h-[150px] rounded-[4px] bg-[#212121] flex flex-col justify-center items-center relative overflow-hidden mx-6`}
+          >
+            <div
+              className={`w-full h-[40%] flex flex-row justify-center items-center`}
+            >
+              <FontAwesomeIcon
+                icon={faShield}
+                className={`transition-all duration-500 h-[50px] w-[50px] text-white/70 m-2 cursor-pointer pt-2`}
+                onClick={() => {}}
+              />
+            </div>
+            <div
+              className={`w-full h-[60%] flex flex-col justify-center items-center`}
+            >
+              <p
+                className={`text-[18px] font-black text-[#4de850]/60 Monts_ mb-1`}
+              >
+                Secure Space
+              </p>
+              <p
+                className={`w-[90%] mx-auto text-[13px] text-white/50 Work_ font-thin text-center relative`}
+              >
+                Magna irure dolor labore nulla eu commodo deserunt labore duis.
+              </p>
+            </div>
+          </div>
+          <div
+            className={`w-[250px] h-[150px] rounded-[4px] bg-[#212121] flex flex-col justify-center items-center relative overflow-hidden mx-6`}
+          >
+            <div
+              className={`w-full h-[40%] flex flex-row justify-center items-center`}
+            >
+              <FontAwesomeIcon
+                icon={faMusic}
+                className={`transition-all duration-500 h-[50px] w-[50px] text-white/70 m-2 cursor-pointer pt-2`}
+                onClick={() => {}}
+              />
+            </div>
+            <div
+              className={`w-full h-[60%] flex flex-col justify-center items-center`}
+            >
+              <p
+                className={`text-[18px] font-black text-[#4de850]/60 Monts_ mb-1`}
+              >
+                International Vibe
+              </p>
+              <p
+                className={`w-[90%] mx-auto text-[13px] text-white/50 Work_ font-thin text-center relative`}
+              >
+                Magna irure dolor labore nulla eu commodo deserunt labore duis.
+              </p>
+            </div>
+          </div>
+          <div
+            className={`w-[250px] h-[150px] rounded-[4px] bg-[#212121] flex flex-col justify-center items-center relative overflow-hidden mx-6`}
+          >
+            <div
+              className={`w-full h-[40%] flex flex-row justify-center items-center`}
+            >
+              <FontAwesomeIcon
+                icon={faPeopleGroup}
+                className={`transition-all duration-500 h-[50px] w-[50px] text-white/70 m-2 cursor-pointer pt-2`}
+                onClick={() => {}}
+              />
+            </div>
+            <div
+              className={`w-full h-[60%] flex flex-col justify-center items-center`}
+            >
+              <p
+                className={`text-[18px] font-black text-[#4de850]/60 Monts_ mb-1`}
+              >
+                Redefining 'Sports Bar'
+              </p>
+              <p
+                className={`w-[90%] mx-auto text-[13px] text-white/50 Work_ font-thin text-center relative`}
+              >
+                Magna irure dolor labore nulla eu commodo deserunt labore duis.
+              </p>
+            </div>
+          </div>
+          <div
+            className={`w-[250px] h-[150px] rounded-[4px] bg-[#212121] flex flex-col justify-center items-center relative overflow-hidden mx-6`}
+          >
+            <div
+              className={`w-full h-[40%] flex flex-row justify-center items-center`}
+            >
+              <FontAwesomeIcon
+                icon={faCheck}
+                className={`transition-all duration-500 h-[50px] w-[50px] text-white/70 m-2 cursor-pointer pt-2`}
+                onClick={() => {}}
+              />
+            </div>
+            <div
+              className={`w-full h-[60%] flex flex-col justify-center items-center`}
+            >
+              <p
+                className={`text-[18px] font-black text-[#4de850]/60 Monts_ mb-1`}
+              >
+                Quality Weed
+              </p>
+              <p
+                className={`w-[90%] mx-auto text-[13px] text-white/50 Work_ font-thin text-center relative`}
+              >
+                Magna irure dolor labore nulla eu commodo deserunt labore duis.
+              </p>
+            </div>
           </div>
         </div>
       </div>
